babf3013474b78e2d9e34909102ae5aeb1dc3b59
wangxin <wangxin0110@hotmail.com>
2018-01-19 15:27:10 +0800

    Merge branch 'master' of https://github.com/wangxin1028/py_study
    # Conflicts:
    #	src/Test.py

--- Student.py
+++ Student.py
@@ -1,6 +1,6 @@
 class Student(object):
     def __init__(self,name,age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
     def say(self):
-        print("My name is %s , My age is %d" % (self.name,self.age))
+        print("My name is %s , My age is %d" % (self._name,self._age))
